1cd0c1c8dfcfe84325718af2dd3d5875618846c2
CNeiheisel <124808780+CNeiheisel@users.noreply.github.com>
2025-10-05 11:50:53 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const { Pool } = require('pg');
-const { spawn } = require('child_process');
+
 const path = require('path');
 
 const app = express();
